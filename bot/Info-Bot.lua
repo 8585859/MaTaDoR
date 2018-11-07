22e1759d1af7b184b9f6388d0eb19035446116f0
8585859 <44847528+8585859@users.noreply.github.com>
2018-11-07 22:04:46 +0330

    Add files via upload

---
+++ bot/Info-Bot.lua
@@ -0,0 +1,15 @@
+bot_token = "793235442:AAH_gqSB6hZGMzV58cX2Xk7RGQsCIioTqyw" -- توکن رباتُ
+channel_username = '@Tiigertaem' -- آیدی کانال
+channel_inline = 'tiigertaem' -- آیدی کانال بدون @
+sudo_username = '@arashm' -- آیدی سازنده ربات
+gp_sudo = -1001349230237 -- آیدی گروه سودو ها
+SUDO = 205549111 -- آیدی سودو اصلی
+BotMaTaDoR_id = 793857379 --آیدی ربات cli
+BotMaTaDoR_idapi = 793235442 --آیدی ربات api
+RedisIndex = '1' -- شماره ردیس
+EndPm = " ツ"
+
+--[[
+opened By @arashwm AND @arashwm
+My Channel @arashwm & @Tiigertaem
+]]--
